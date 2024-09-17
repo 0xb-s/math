0da0cc996da967062f1375c5d722747c46916d0f
0xb-s <145866191+0xb-s@users.noreply.github.com>
2024-09-17 05:38:54 -0700

    Create ast.rs

---
+++ src/ast.rs
@@ -0,0 +1,11 @@
+
+
+#[derive(Debug, Clone)]
+pub enum Expr {
+    Number(f64),
+    Variable(String),
+    Add(Box<Expr>, Box<Expr>),
+    Sub(Box<Expr>, Box<Expr>),
+    Mul(Box<Expr>, Box<Expr>),
+    Div(Box<Expr>, Box<Expr>),
+}
